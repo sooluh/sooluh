a5788a35aea367a0002c50e88aa6185770c9b18d
Torch <20874779+sooluh@users.noreply.github.com>
2023-04-19 16:49:02 +0700

    :memo: update ayahaday.ts

--- src/ayahaday.ts
+++ src/ayahaday.ts
@@ -1,7 +1,7 @@
 const request = await fetch('https://api.tarteel.io/v1/aad/schedule/')
 const json = await request.json()
 
-const template = `<sub>_${json.surahNameEnTrans}_</sub><br>\n**Surah ${json.surahNameEn}** (${json.surah}: ${json.ayah})\n\n> ${json.englishTranslation}\n\n\\— ${json.hijriDate}`
+const template = `<sub>_${json.surahNameEnTrans}_</sub><br>\n**Surah ${json.surahNameEn}** (${json.surah}: ${json.ayah})\n\n> ${json.englishTranslation}\n\n— ${json.hijriDate}`
 const markdown = await Deno.readTextFile('./README.md')
 
 const regex = /^(<!--(?:\s|)AYAHADAY:(?:START|start)(?:\s|)-->)(?:\n|)([\s\S]*?)(?:\n|)(<!--(?:\s|)AYAHADAY:(?:END|end)(?:\s|)-->)$/gm
